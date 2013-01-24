23f673c9f12c8b12d71c642a52ef29dab6f91eaa
Watson <watson1978@gmail.com>
2013-01-24 10:21:48 +0900

    clean up

--- lib/motion-pixate-observer.rb
+++ lib/motion-pixate-observer.rb
@@ -5,6 +5,5 @@ end
 lib_dir_path = File.dirname(File.expand_path(__FILE__))
 Motion::Project::App.setup do |app|
   app.files.concat(Dir.glob(File.join(lib_dir_path, "project/**/*.rb")))
-
   app.vendor_project(File.join(lib_dir_path, "Socket"), :static)
 end
